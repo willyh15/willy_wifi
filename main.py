c94800eb5f5dc64d6146af4d824ede65472911f6
willyh15 <willharrison2016@gmail.com>
2023-12-26 20:47:32 -0600

    updated

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-# main.py
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.lang import Builder
-# Import your screens here
+from kivymd.uix.screen import MDScreen
 
-class MyApp(MDApp):
-    sm = ScreenManager()
+# Import your screen classes
+from interfacetoolscreen import InterfaceToolScreen
+from pysharkscreen import PySharkScreen
+from tsharkscreen import TSharkScreen
+from ghostmodescreen import GhostModeScreen
+from postexploitationscreen import PostExploitationScreen
+
+class MainApp(MDApp):
+    def build(self):
+        # Create a ScreenManager
+        sm = ScreenManager()
+
+        # Add your screens
+        sm.add_widget(InterfaceToolScreen(name='interfacetool'))
+        sm.add_widget(PySharkScreen(name='pyshark'))
+        sm.add_widget(TSharkScreen(name='tshark'))
+        sm.add_widget(GhostModeScreen(name='ghostmode'))
+        sm.add_widget(PostExploitationScreen(name='postexploitation'))
+
+        return sm
+
+    def on_start(self):
+        # Any additional startup logic if needed
+        pass
+
 if __name__ == '__main__':
-    MyApp().run()
+    MainApp().run()
