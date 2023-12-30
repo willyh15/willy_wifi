b840fed02ab27cc8643ebda949e69cefb15650de
willyh15 <willharrison2016@gmail.com>
2023-12-30 00:33:21 -0600

    updated

--- interfacetoolscreen.py
+++ interfacetoolscreen.py
@@ -4,7 +4,12 @@ from kivymd.uix.screen import MDScreen
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.metrics import dp
-from utils import get_available_ssids, get_interfaces
+import subprocess
+import openai
+import re
+
+from utils import get_interfaces
+
 
 class InterfaceToolScreen(MDScreen):
     def __init__(self, **kwargs):
@@ -42,7 +47,7 @@ class InterfaceToolScreen(MDScreen):
 
     def refresh_interfaces(self):
         try:
-            interfaces = get_interfaces()
+            interfaces = self.get_interfaces()
         except Exception as e:
             print(f"An error occurred while getting interfaces: {e}")
             return
@@ -63,7 +68,7 @@ class InterfaceToolScreen(MDScreen):
     def refresh_ssids(self):
         if self.selected_interface:
             try:
-                ssids = get_available_ssids(self.selected_interface)
+                ssids = self.get_available_ssids(self.selected_interface)
             except Exception as e:
                 print(f"An error occurred while getting SSIDs: {e}")
                 return
@@ -86,3 +91,47 @@ class InterfaceToolScreen(MDScreen):
     def show_ssid_dropdown(self):
         if self.ssid_menu:
             self.ssid_menu.open()
+
+    def validate_ip_address(ip_addr):
+        # A basic regular expression for IP address validation
+        ip_pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3}(-\d{1,3})?$')
+        return ip_pattern.match(ip_addr) is not None
+
+    # Function to list available network interfaces
+    def get_interfaces(self):
+        cmd = ["ip", "link", "show"]
+        result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if result.returncode != 0:
+            print("Failed to get interfaces:", result.stderr)
+            return []
+        interfaces = [line.split(":")[1].strip() for line in result.stdout.split('\n') if
+                      'state' in line and not 'DOWN' in line]
+        return interfaces
+
+    # Function to scan for available SSIDs
+    def get_available_ssids(interface):
+        try:
+            scan_results = subprocess.check_output(['iwlist', interface, 'scan'], text=True)
+            ssids = re.findall(r'ESSID:"([^"]+)"', scan_results)
+            return list(set(ssids))
+        except subprocess.CalledProcessError as e:
+            print("Error scanning for SSIDs:", e)
+            return []
+
+    # Function to analyze live packets
+    def analyze_packet(self):
+        pass
+
+    def send_prompt_to_language_model(prompt):
+        try:
+            response = openai.Completion.create(
+                engine="davinci",
+                prompt=prompt,
+                max_tokens=150
+            )
+
+            return response.choices[0].text.strip()
+        except Exception as e:
+            print(f"Error in querying the language model: {e}")
+            return "Error in generating response."
+
